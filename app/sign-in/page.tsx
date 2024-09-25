853125fe03969e84e6b3cfb74d8ec601a8285fe6
Norberto Caceres <norberto.e.888@gmail.com>
2024-09-25 00:18:04 -0500

    basic conditional layout

--- app/sign-in/page.tsx
+++ app/sign-in/page.tsx
@@ -24,7 +24,7 @@ export default async function SignIn() {
             await signOut();
           }}
         >
-          <button type="submit">
+          <button type="submit" className="border border-indigo-600">
             Sign Out ({session.user.email}, {session.user.role})
           </button>
         </form>
