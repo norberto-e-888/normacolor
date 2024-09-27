912ad37a4ba23793303ab343ce8206abc0fe8200
Norberto Caceres <norberto.e.888@gmail.com>
2024-09-26 20:13:21 -0500

    install @neshca/cache-handler and add commented out example code of how to use neshCache in a server action

--- app/(private)/(client)/page.tsx
+++ app/(private)/(client)/page.tsx
@@ -1,3 +1,48 @@
-export default function ClientHomePage() {
-  return <main>Client home page</main>;
+/* import { neshCache } from "@neshca/cache-handler/functions";
+
+type Query = Record<string, unknown>;
+
+const getData = neshCache(
+  async (query: Query) => {
+    console.log(
+      { query },
+      "In the real implementation we would use the query object in out db querying logic"
+    );
+
+    return ["test data"];
+  },
+  {
+    argumentsSerializer: ([query]) => `posts:${JSON.stringify(query)}`,
+    revalidate: 1000 * 60 * 30,
+  }
+);
+
+const fetch = async (query: Query) => {
+  const psk = `posts:${JSON.stringify(query)}`;
+  const data = await getData(
+    {
+      tags: ["posts", psk],
+    },
+    query
+  );
+
+  return data;
+};
+ */
+export default async function ClientHomePage() {
+  /*   const data = await fetch({
+    filters: {
+      category: "someCategory",
+    },
+    pagination: {
+      page: 1,
+      size: 10,
+    },
+    sort: {
+      by: "createdAt",
+      order: "desc",
+    },
+  }); */
+
+  return <main>Client home page{/*  {JSON.stringify(data)} */}</main>;
 }
