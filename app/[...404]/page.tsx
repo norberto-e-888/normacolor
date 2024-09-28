1f0689f1e3976172135be90a10cd9f391dff0abe
Norberto Caceres <norberto.e.888@gmail.com>
2024-09-28 00:42:40 -0500

    rename catch all page to 404

--- app/[...404]/page.tsx
+++ app/[...404]/page.tsx
@@ -3,7 +3,7 @@ import { redirect } from "next/navigation";
 import { UserRole } from "@/database";
 import { getServerSession } from "@/functions/auth";
 
-export default async function CatchAllPage() {
+export default async function NotFoundPage() {
   const session = await getServerSession();
 
   if (session) {
