f7f574f0373e53521dcc6cc2271ddb9e5ff16e7e
Norberto Caceres <norberto.e.888@gmail.com>
2024-09-29 03:03:04 -0500

    add setAdminPassword server action

--- functions/auth/index.ts
+++ functions/auth/index.ts
@@ -1,3 +1,4 @@
 export { getServerSession } from "./get-server-session";
+export { setAdminPassword } from "./set-admin-password";
 export { signInWithMagicLink } from "./sign-in-with-magic-link";
 export { signOut } from "./sign-out";
