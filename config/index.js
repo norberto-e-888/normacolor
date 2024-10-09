9088d97dc9f8ca95b3eb755697fa3f6b726bfe8c
Norberto Caceres <norberto.e.888@gmail.com>
2024-10-09 17:26:07 -0500

    make NGROK_TOKEN and NGROK_TUNNEL optional

--- config/index.js
+++ config/index.js
@@ -6,8 +6,8 @@ const Config = z.object({
   AUTH_RESEND_KEY: z.string(),
   AUTH_SENDGRID_KEY: z.string(),
   MONGODB_URI: z.string(),
-  NGROK_TOKEN: z.string(),
-  NGROK_TUNNEL: z.string(),
+  NGROK_TOKEN: z.string().optional(),
+  NGROK_TUNNEL: z.string().optional(),
 });
 
 const config = Config.parse(process.env);
