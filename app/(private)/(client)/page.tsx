1918ca972094d4fa9a49a1af57245fcdb93461a8
Norberto Caceres <norberto.e.888@gmail.com>
2025-01-14 17:27:43 -0500

    trivial

--- app/(private)/(client)/page.tsx
+++ app/(private)/(client)/page.tsx
@@ -1,58 +1,9 @@
-/* import { createClient } from "redis";
-import { neshCache } from "@neshca/cache-handler/functions";
+import { Content } from "@/components/ui/content";
 
-type Query = Record<string, unknown>;
-
-const redis = createClient();
-const createPSK = (resource: string, query: Query) =>
-  `${resource}:${JSON.stringify(query)}`;
-
-const cachedGetData = neshCache(
-  async (query: Query) => {
-    console.log(
-      { query },
-      "In the real implementation we would use the query object in out db querying logic"
-    );
-
-    const documents = await (async () => [{ id: 1 }, { id: 2 }])();
-    await Promise.all(
-      documents.map(({ id }) =>
-        redis.sAdd(`IQI:posts:${id}`, createPSK("posts", query))
-      )
-    );
-
-    return documents;
-  },
-  {
-    argumentsSerializer: ([query]) => `posts:${JSON.stringify(query)}`,
-    revalidate: 1000 * 60 * 30,
-  }
-);
-
-const handleFetchDocument = async (query: Query) => {
-  const psk = `posts:${JSON.stringify(query)}`;
-  return cachedGetData(
-    {
-      tags: ["posts", psk],
-    },
-    query
-  );
-};
- */
 export default async function ClientHomePage() {
-  /*   const data = await handleFetchDocument({
-    filters: {
-      category: "someCategory",
-    },
-    pagination: {
-      page: 1,
-      size: 10,
-    },
-    sort: {
-      by: "createdAt",
-      order: "desc",
-    },
-  });
- */
-  return <main>Client home page{/* {JSON.stringify(data)} */}</main>;
+  return (
+    <Content>
+      <h1>Client Dashboard</h1>
+    </Content>
+  );
 }
