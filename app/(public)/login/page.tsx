1d4678c9ace2079ba64f69e52df4318c722ed6c0
Norberto Caceres <norberto.e.888@gmail.com>
2025-01-14 17:45:40 -0500

    fix infinite redirect loop

--- app/(public)/login/page.tsx
+++ app/(public)/login/page.tsx
@@ -34,7 +34,7 @@ export default function LoginPage() {
 
     if (status === "authenticated") {
       timeout = setTimeout(() => {
-        router.replace("/");
+        router.replace("/productos");
       }, 800);
     }
 
