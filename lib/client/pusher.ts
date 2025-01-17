2f2100eef22ac9abe11667964dec768a25bb9de3
Norberto Caceres <norberto.e.888@gmail.com>
2025-01-17 12:56:15 -0500

    trivial

--- lib/client/pusher.ts
+++ lib/client/pusher.ts
@@ -1,12 +1,5 @@
 import PusherClient from "pusher-js";
 
-console.log("Initializing Pusher client with:", {
-  appKey: process.env.NEXT_PUBLIC_PUSHER_APP_KEY,
-  cluster: process.env.NEXT_PUBLIC_PUSHER_CLUSTER,
-});
-
-PusherClient.logToConsole = process.env.NODE_ENV === "development";
-
 export const pusherClient = new PusherClient(
   process.env.NEXT_PUBLIC_PUSHER_APP_KEY!,
   {
