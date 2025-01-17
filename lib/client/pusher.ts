008500ef8e44a69af84ada11650e194e02458467
Norberto Caceres <norberto.e.888@gmail.com>
2025-01-17 11:13:44 -0500

    fix pusher config

--- lib/client/pusher.ts
+++ lib/client/pusher.ts
@@ -4,5 +4,6 @@ export const pusherClient = new PusherClient(
   process.env.NEXT_PUBLIC_PUSHER_APP_ID!,
   {
     cluster: process.env.NEXT_PUBLIC_PUSHER_CLUSTER!,
+    forceTLS: true,
   }
 );
--- lib/server/pusher.ts
+++ lib/server/pusher.ts
@@ -2,7 +2,7 @@ import PusherServer from "pusher";
 
 export const pusherServer = new PusherServer({
   appId: process.env.PUSHER_APP_ID!,
-  key: process.env.NEXT_PUBLIC_PUSHER_APP_ID!,
+  key: process.env.PUSHER_API_KEY!,
   secret: process.env.PUSHER_API_SECRET!,
   cluster: process.env.PUSHER_CLUSTER!,
   useTLS: true,
